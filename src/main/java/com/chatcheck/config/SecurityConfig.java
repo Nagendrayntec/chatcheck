74e1f71d476af6b2c1c3f224d4e23923e8dd5082
Nagendra-yntec <nagendra.v@yntec.in>
2026-04-07 15:01:40 +0530

    Update SecurityConfig.java

--- src/main/java/com/chatcheck/config/SecurityConfig.java
+++ src/main/java/com/chatcheck/config/SecurityConfig.java
@@ -3,10 +3,31 @@ package com.chatcheck.config;
 import org.springframework.context.annotation.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+package com.chatcheck.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 @Configuration
-public class SecurityConfig{
- @Bean
- public BCryptPasswordEncoder encoder(){
-  return new BCryptPasswordEncoder();
- }
+public class SecurityConfig {
+
+@Bean
+public BCryptPasswordEncoder encoder(){
+ return new BCryptPasswordEncoder();
+}
+
+@Bean
+public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+ http
+  .csrf(csrf->csrf.disable())
+  .authorizeHttpRequests(auth->auth
+   .anyRequest().permitAll()
+  );
+
+ return http.build();
+}
+
 }
