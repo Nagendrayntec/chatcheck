b57996a077898a68474d4b39d79257e86248fa43
Nagendra-yntec <nagendra.v@yntec.in>
2026-04-07 14:32:09 +0530

    Add main Spring Boot class

---
+++ src/main/java/com/chatcheck/ChatcheckApplication.java
@@ -0,0 +1,11 @@
+package com.chatcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatcheckApplication {
+ public static void main(String[] args){
+  SpringApplication.run(ChatcheckApplication.class,args);
+ }
+}
